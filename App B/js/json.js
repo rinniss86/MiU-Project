d8dbd52f85507b0b961e4b5c44ae8fbabd6347dc
rinniss86 <rinniss86@gmail.com>
2012-03-08 23:11:40 -0500

    Re added Files

---
+++ App B/js/json.js
@@ -0,0 +1,225 @@
+var json = {
+	"order1":{
+		"name": ["Name:", "Rich"],
+		"email": ["Email:", "rinniss86@fullsail.edu"],
+		"phone": ["Phone:", "(914)9359476"],
+		"crave": ["Craving:", "chinese"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "8"],
+		"date": ["Date:", "2012-02-20"],
+		"where": ["How/Where:", "Sit Down"],
+		"comment": ["Instructions:", "Sesame Shrimp"]
+	},
+	"order2":{
+		"name": ["Name:", "Alli"],
+		"email": ["Email:", "alligootz@msn.com"],
+		"phone": ["Phone:", "(914)9373687"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "4"],
+		"date": ["Date:", "2012-02-19"],
+		"where": ["How/Where:", "Sit Down"],
+		"comment": ["Instructions:", "Empanadas"]
+	},
+	"order3":{
+		"name": ["Name:", "Brian"],
+		"email": ["Email:", "brianrocks@aol.com"],
+		"phone": ["Phone:", "(914)6908754"],
+		"crave": ["Craving:", "american"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "10"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Sit Down"],
+		"comment": ["Instructions:", "Bacon Cheese Burger"]
+	},
+	"order4":{
+		"name": ["Name:", "Joe"],
+		"email": ["Email:", "joe@aol.com"],
+		"phone": ["Phone:", "(914)1231234"],
+		"crave": ["Craving:", "italian"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "10"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Sit Down"],
+		"comment": ["Instructions:", "Chicken Parm"]
+	},
+	"order5":{
+		"name": ["Name:", "Alison"],
+		"email": ["Email:", "Ali@aol.com"],
+		"phone": ["Phone:", "(914)8908534"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "6"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Sit Down"],
+		"comment": ["Instructions:", "Jerk Chiken"]
+	},
+	"order6":{
+		"name": ["Name:", "Bryan"],
+		"email": ["Email:", "ZOMG@aol.com"],
+		"phone": ["Phone:", "(914)6908564"],
+		"crave": ["Craving:", "other"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "10"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Cook Your Own"],
+		"comment": ["Instructions:", "Jerk Chiken"]
+	},
+	"order7":{
+		"name": ["Name:", "Lisa"],
+		"email": ["Email:", "LisaLoops@aol.com"],
+		"phone": ["Phone:", "(917)6908564"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "4"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Cook Your Own"],
+		"comment": ["Instructions:", "Empanadas"]
+	},
+	"order8":{
+		"name": ["Name:", "Sam"],
+		"email": ["Email:", "SamWise@yahoo.com"],
+		"phone": ["Phone:", "(914)7548564"],
+		"crave": ["Craving:", "american"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "3"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Cook your Own"],
+		"comment": ["Instructions:", "Grilled Cheese"]
+	},	
+	"order9":{
+		"name": ["Name:", "Felicity"],
+		"email": ["Email:", "FullofIT@yahoo.com"],
+		"phone": ["Phone:", "(914)7537024"],
+		"crave": ["Craving:", "chinese"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "7"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Cook your Own"],
+		"comment": ["Instructions:", "Chicken and Broccoli"]
+	},	
+	"order10":{
+		"name": ["Name:", "Jenny"],
+		"email": ["Email:", "jBlock@yahoo.com"],
+		"phone": ["Phone:", "(914)7528642"],
+		"crave": ["Craving:", "indian"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "8"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Cook your Own"],
+		"comment": ["Instructions:", "Curry Goat"]
+	},
+	"order11":{
+		"name": ["Name:", "George"],
+		"email": ["Email:", "george@fullsail.edu"],
+		"phone": ["Phone:", "(914)9358531"],
+		"crave": ["Craving:", "chinese"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "9"],
+		"date": ["Date:", "2012-02-20"],
+		"where": ["How/Where:", "Delivery"],
+		"comment": ["Instructions:", "Sesame Chicken"]
+	},
+	"order12":{
+		"name": ["Name:", "Bobby"],
+		"email": ["Email:", "BFLAY@msn.com"],
+		"phone": ["Phone:", "(203)9367387"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "6"],
+		"date": ["Date:", "2012-02-19"],
+		"where": ["How/Where:", "Delivery"],
+		"comment": ["Instructions:", "Rice and Black Beans"]
+	},
+	"order13":{
+		"name": ["Name:", "Jesus"],
+		"email": ["Email:", "JFish@aol.com"],
+		"phone": ["Phone:", "(718)4839212"],
+		"crave": ["Craving:", "american"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "5"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Delivery"],
+		"comment": ["Instructions:", "Chicken Nuggets"]
+	},
+	"order14":{
+		"name": ["Name:", "Jerry"],
+		"email": ["Email:", "JWood@opt.com"],
+		"phone": ["Phone:", "(765)1231234"],
+		"crave": ["Craving:", "italian"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "8"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Delivery"],
+		"comment": ["Instructions:", "Chicken Cutlets"]
+	},
+	"order15":{
+		"name": ["Name:", "Barry"],
+		"email": ["Email:", "Bmudz78@aol.com"],
+		"phone": ["Phone:", "(914)5408534"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "no"],
+		"hunger": ["How Hungry", "9"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Delivery"],
+		"comment": ["Instructions:", "Chicken Gyro"]
+	},
+	"order16":{
+		"name": ["Name:", "Erin"],
+		"email": ["Email:", "ringer8976@aol.com"],
+		"phone": ["Phone:", "(914)8458564"],
+		"crave": ["Craving:", "other"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "10"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Pick Up"],
+		"comment": ["Instructions:", "Poptarts"]
+	},
+	"order17":{
+		"name": ["Name:", "Cindy"],
+		"email": ["Email:", "CindyHorsey123@aol.com"],
+		"phone": ["Phone:", "(917)6208424"],
+		"crave": ["Craving:", "spanish"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "9"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Pick Up"],
+		"comment": ["Instructions:", "Papa Rellena"]
+	},
+	"order18":{
+		"name": ["Name:", "Kim"],
+		"email": ["Email:", "Kimypoo@yahoo.com"],
+		"phone": ["Phone:", "(914)1953564"],
+		"crave": ["Craving:", "american"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "10"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Pick Up"],
+		"comment": ["Instructions:", "BBQ Ribs"]
+	},	
+	"order19":{
+		"name": ["Name:", "Jessica"],
+		"email": ["Email:", "JayLady12453@yahoo.com"],
+		"phone": ["Phone:", "(516)2035024"],
+		"crave": ["Craving:", "chinese"],
+		"fav": ["Favorite?:", "Yes"],
+		"hunger": ["How Hungry", "8"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Pick Up"],
+		"comment": ["Instructions:", "Beef and Broccoli"]
+	},	
+	"order20":{
+		"name": ["Name:", "Mike"],
+		"email": ["Email:", "MikeBoom@yahoo.com"],
+		"phone": ["Phone:", "(212)3947204"],
+		"crave": ["Craving:", "italian"],
+		"fav": ["Favorite?:", "yes"],
+		"hunger": ["How Hungry", "8"],
+		"date": ["Date:", "2012-02-24"],
+		"where": ["How/Where:", "Pick Up"],
+		"comment": ["Instructions:", "Chicken Alfredo"]
+	},	
+	
+
+
+}
