9e38c63edbae98e924a9ca15a87dc0ab9a725810
rinniss86 <rinniss86@gmail.com>
2012-03-14 19:53:44 -0400

    Form Validations. Not everything is working correctly.

--- AppA/js/main.js
+++ AppA/js/main.js
@@ -6,9 +6,21 @@ var parseOrderForm = function(data){
 $(document).ready(function(){
 	
 	var oform = $('#form');
+	oferrorslink = $('#oferrorslink')
+	;
 	
 	oform.validate({
-		invalidHandler: function(form, validator){},
+		invalidHandler: function(form, validator){
+			oferrorslink.click();
+			var html = '';
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not('[generated]');
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += '<li>' + fieldName + '</li>';
+			};
+			$("#errors ul").html(html);
+		},
 		submitHandler: function(){
 			var data = oform.serializeArray();
 			parseOrderForm(data);
